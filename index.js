0c1aca52ca50237468f1556b7343058c27f1ee70
qAbdullajon <azikmelor7705@gmail.com>
2025-01-27 00:36:33 +0500

    API weather

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 import getArgs from "./helpers/args.js";
+import getWeather from "./services/api.service.js";
 import { printError, printHelp, printSuccess } from "./services/log.service.js";
+import { saveKeyValue, TOKEN_DICTIONARY } from "./services/storage.service.js";
+
+const saveToken = async (token) => {
+  if (!token.length) {
+    printError("Token doesn't exist!");
+    return;
+  }
+  try {
+    await saveKeyValue(TOKEN_DICTIONARY.token, token);
+    printSuccess("Token was saved");
+  } catch (error) {
+    printError(error.message);
+  }
+};
 
 const startCli = () => {
   const args = getArgs(process.argv);
@@ -13,8 +28,10 @@ const startCli = () => {
   }
   if (args.t) {
     // save token
+    return saveToken(args.t);
   }
   // result
+  getWeather("Tashkent");
 };
 
 startCli();
---
+++ services/api.service.js
@@ -0,0 +1,24 @@
+import https from "https";
+import axios from "axios";
+import { getKeyValue, TOKEN_DICTIONARY } from "./storage.service.js";
+
+const getWeather = async (city) => {
+  // https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+
+  const token = await getKeyValue(TOKEN_DICTIONARY.token);
+  if (!token) {
+    throw new Error("API doesn't exist, -t [API_KEY] for saving token");
+  }
+
+  const { data } = await axios.get("https://api.openweathermap.org/data/2.5/weather", {
+    params: {
+      q: city,
+      appid: token,
+      lang: "en",
+      units: "metric",
+    },
+  });
+  return data;
+};
+
+export default getWeather;
